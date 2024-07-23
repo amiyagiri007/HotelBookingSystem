3e98324173fb2cc629e5d6c275b46a87d1e06e70
amiyagiri007 <amiyakrushnagiri430@gmail.com>
2024-07-23 17:12:00 +0530

    VO class

---
+++ HotelBookingSystem/src/main/java/com/hotel/vo/UserVO.java
@@ -0,0 +1,14 @@
+package com.hotel.vo;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class UserVO {
+	private String userId;
+	private String userEmail;
+	private String userName;
+	private String userPassword;
+
+}
