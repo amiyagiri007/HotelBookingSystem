9611d8c6cd926eb65e146fa799dce655eb8ed548
amiyagiri007 <amiyakrushnagiri430@gmail.com>
2024-07-31 11:46:40 +0530

    No Changes

--- HotelBookingSystem/src/main/java/com/hotel/entity/User.java
+++ HotelBookingSystem/src/main/java/com/hotel/entity/User.java
@@ -7,7 +7,6 @@ import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 
-import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.FetchType;
@@ -16,7 +15,6 @@ import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
-import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.Size;
